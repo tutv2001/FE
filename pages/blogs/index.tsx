817cf566885eee0e8100410cb11f9eddd94ead12
Lưu Tiến Anh <tienanhluu233574@gmail.com>
2022-11-21 21:17:22 +0700

    cart ui

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -28,38 +28,7 @@ const index = ({ posts, catePost }: Props) => {
       </ul>
 
       <>
-  {/* component */}
-  {/* <section className="bg-white dark:bg-gray-900">
-    <div className="container px-6 py-10 mx-auto">
-      <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
-        From the blog
-      </h1>
-      <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
 
-    {posts.map((item, index) => (
-              <div className="lg:flex" key={index}>
-              <img
-                className="object-cover w-full h-56 rounded-lg lg:w-64"
-                src={item.thumbnail}
-                alt=""
-              />
-              <div className="flex flex-col justify-between py-6 lg:mx-6">
-                <a
-                  href="#"
-                  className="text-xl font-semibold text-gray-800 hover:underline dark:text-white "
-                >
-                  {item.title}
-                </a>
-                <span className="text-sm text-gray-500 dark:text-gray-300">
-                {item.content}
-                </span>
-              </div>
-            </div>
-    ))}
-      
-      </div>
-    </div>
-  </section> */}
         <div className="bg-gray-100 dark:bg-gray-900 w-[1410px] mx-auto">
   <div className="container mt-12">
     <div className="flex relative rounded-md overflow-hidden bg-blue-400">
