7fddb2ea5f4061c68d452e845d175dcbc9802bb4
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-11-24 19:59:47 +0700

    feact profile

--- pages/user/changePassword.tsx
+++ pages/user/changePassword.tsx
@@ -1,9 +1,41 @@
+import { useRouter } from 'next/router';
 import React from 'react'
+import {SubmitHandler, useForm } from 'react-hook-form';
+import { useSelector } from 'react-redux';
 import UserNav from '../../component/user-nav'
-
+import { Tuser } from '../../models/user';
+import { RootState } from '../../redux/store';
+import { toast } from "react-toastify";
+import { changePass } from '../../Api/userApi';
+import { NextPageWithLayout } from '../../models/layout';
 type Props = {}
+type Inputs = {
+    _id: string;
+    oldPassword: string;
+    newPassword: string;
+    createdAt?: Date;
+  };
 
-const changePassword = (props: Props) => {
+const ChangePassword :NextPageWithLayout  = (props: Props) => {
+    const currentUser = useSelector((state: RootState) => state.auth.currentUser) as Tuser;
+    console.log(currentUser._id);
+    const router = useRouter();
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<Inputs>();
+  
+    const onSubmit: SubmitHandler<Inputs> = async (values: Inputs) => {
+      try {
+        await changePass(values);
+        toast.success("Đổi thành công, vui lòng đăng nhập");
+        router.push("/");
+      } catch (error) {
+        console.log(error);
+        toast.error("Có lỗi xảy ra, vui lòng thử lại");
+      }
+    };
     return (
         <div>
             <div className="main w-[1410px] mx-auto mt-[80px]">
@@ -12,17 +44,20 @@ const changePassword = (props: Props) => {
                     <UserNav />
                     <div className='ml-[140px]'>
                         <h2 className='text-xl'>Đổi mật khẩu</h2>
-                        <div className='mt-[14px]'>
+                        <form className='mt-[14px]' onSubmit={handleSubmit(onSubmit)}>
+                            <input type="text" {...register("_id")} className="hidden" value={currentUser._id} />
                             <div className='mb-8'>
                                 <label htmlFor="">Mật khẩu cũ</label><br />
-                                <input type="text" className='mt-2 w-[400px] h-[40px] border'/>
+                                <input type="text"  {...register("oldPassword", { required: "Vui lòng" })} className='mt-2 w-[400px] h-[40px] border'/>
+                                <p className="text-red-400">{errors.oldPassword?.message}</p>
                             </div>
                             <div>
                                 <label htmlFor="">Mật khẩu mới</label><br />
-                                <input type="text" className='mt-2 w-[400px] h-[40px] border'/>
+                                <input type="text" {...register("newPassword", { required: "Vui lòng nhập họ tên" })} className='mt-2 w-[400px] h-[40px] border'/>
+                                <p className="text-red-400">{errors.newPassword?.message}</p>
                             </div>
                             <button className='bg-black text-white text-center w-[160px] h-[38px] mt-4 rounded-full'>Đổi mật khẩu</button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -30,4 +65,4 @@ const changePassword = (props: Props) => {
     )
 }
 
-export default changePassword
+export default ChangePassword
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import UserNav from '../../component/user-nav'
+import { Tuser } from '../../models/user';
+import { RootState } from '../../redux/store';
 
 type Props = {}
 
 const User = (props: Props) => {
+    const currentUser = useSelector((state: RootState) => state.auth.currentUser) as Tuser;
     return (
         <div>
             <div className="main w-[1410px] mx-auto mt-[80px]">
@@ -13,9 +17,9 @@ const User = (props: Props) => {
                     <div className='ml-[140px]'>
                         <h2 className='text-xl'>Thông tin tài khoản</h2>
                         <div className='mt-[14px]'>
-                            <div className='text-lg'><a href="" >Họ và tên: </a> <a href="">Admin</a></div>
-                            <div className='text-lg'><a href="" >Email: </a> <a href="">admin@gmail.com</a></div>
-                            <div className='text-lg'><a href="" >Số điện thoại: </a> <a href="">0345236586</a></div>
+                            <div className='text-lg'><a href="" >Họ và tên: </a> <a href="">{currentUser.name}</a></div>
+                            <div className='text-lg'><a href="" >Email: </a> <a href="">{currentUser.email}</a></div>
+                            <div className='text-lg'><a href="" >Số điện thoại: </a> <a href="">{currentUser.phone}</a></div>
 
                         </div>
                     </div>
